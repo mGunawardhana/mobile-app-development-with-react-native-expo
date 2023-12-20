02dc4cb44955f862d377f20463960d153e8dded7
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-12-20 20:34:46 +0530

    learning react native button

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { View, Text, Image, ImageBackground, ScrollView } from "react-native";
+import { View, Button } from "react-native";
 
 const logoImg = require("./assets/adaptive-icon.png");
 
@@ -6,42 +6,9 @@ export default function App() {
   /**we can use inner view components in advanced user interfaces*/
   return (
     <View style={{ flex: 1, backgroundColor: "white", padding: 60 }}>
-      {/* if we want to scroll we can use scrollview component another you need
-      unscrolled behaviour you can use normal view component  */}
-      <ScrollView>
-        {/* <Text>
-        <Text style={{ color: "white" }}>Hello</Text>World
-      </Text> */}
-
-        {/* <Image source={logoImg} style={{ width: 300, height: 300 }} /> */}
-
-        {/* network image */}
-        {/* <Image
-        source={{ uri: "https://picsum.photos/300" }}
-        style={{ width: 300, height: 300 }}
-      /> */}
-
-        {/* if we are using flex:1 we can set image into full background */}
-        {/* <ImageBackground source={logoImg} style={{ flex: 1 }}>
-        <Text>Image text</Text>
-      </ImageBackground> */}
-
-        <Image source={logoImg} style={{ width: 300, height: 300 }} />
-
-        <Text>
-          Simple means something is easy and uncomplicated, without too many
-          steps to follow. It can also mean having few parts or features, not
-          complicated or involved.Simple means something is easy and
-          uncomplicated, without too many steps to follow. It can also mean
-          having few parts or features, not complicated or involved.Simple means
-          something is easy and uncomplicated, without too many steps to follow.
-          It can also mean having few parts or features, not complicated or
-          involved.Simple means something is easy and uncomplicated, without too
-          many steps to follow. It can also mean having few parts or features,
-          not complicated or involved.
-        </Text>
-        <Image source={logoImg} style={{ width: 300, height: 300 }} />
-      </ScrollView>
+      {/* the button component allows users to trigger actions*/}
+      {/* the button component has platform-specific rendering for ios and android */}
+      <Button title="Press" onPress={() => console.log("Button pressed!")} />
     </View>
   );
 }
