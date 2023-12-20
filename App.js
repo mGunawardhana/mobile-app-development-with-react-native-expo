52cdf700d7af0e16fa484e90093aeb03c6b67cd9
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-12-20 20:04:40 +0530

    learning set image getting from gallery and getting network image from url

--- App.js
+++ App.js
@@ -1,15 +1,23 @@
-import { View } from "react-native";
+import { View, Text, Image } from "react-native";
+
+const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
   /**we can use inner view components in advanced user interfaces*/
   return (
-    <View style={{ flex: 1, backgroundColor: "plum" }}>
-      <View
-        style={{ width: 200, height: 200, backgroundColor: "lightblue" }}
-      ></View>
-      <View
-        style={{ width: 200, height: 200, backgroundColor: "lightgreen" }}
-      ></View>
+    <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
+      {/* <Text>
+        <Text style={{ color: "white" }}>Hello</Text>World
+      </Text> */}
+
+      <Image source={logoImg} style={{ width: 300, height: 300 }} />
+
+      {/* network image */}
+      <Image
+        source={{ uri: "https://picsum.photos/300" }}
+        style={{ width: 300, height: 300 }}
+      />
     </View>
+    /** i'll stoped in 9th video*/
   );
 }
