85ace95c99c393f31b4ff5c3cd2fff2d502e1838
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-12-20 22:08:12 +0530

    learning events like onPress

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { View, Button } from "react-native";
+import { View, Button, Image, Text, Pressable } from "react-native";
 
 const logoImg = require("./assets/adaptive-icon.png");
 
@@ -13,6 +13,29 @@ export default function App() {
         onPress={() => console.log("Button pressed!")}
         color="midnightblue"
       />
+      <Pressable onLongPress={() => console.log("image pressed!")}>
+        <Image source={logoImg} style={{ width: 200, height: 200 }} />
+      </Pressable>
+      <Pressable onPress={() => console.log("text pressed!")}>
+        <Text>
+          Learn to read, write, and speak English sayings, slang, new words,
+          more. US and British. A fun, conversational program to write, read,
+          and speak better conversational English. You don't sound odd to us.
+          Hear it at no charge. Get the podcast app. Coast-to-coast US radio.
+          Learn to read, write, and speak English sayings, slang, new words,
+          more. US and British. A fun, conversational program to write, read,
+          and speak better conversational English. You don't sound odd to us.
+          Hear it at no charge. Get the podcast app. Coast-to-coast US radio.
+          Learn to read, write, and speak English sayings, slang, new words,
+          more. US and British. A fun, conversational program to write, read,
+          and speak better conversational English. You don't sound odd to us.
+          Hear it at no charge. Get the podcast app. Coast-to-coast US radio.
+          Learn to read, write, and speak English sayings, slang, new words,
+          more. US and British. A fun, conversational program to write, read,
+          and speak better conversational English. You don't sound odd to us.
+          Hear it at no charge. Get the podcast app. Coast-to-coast US radio.
+        </Text>
+      </Pressable>
     </View>
   );
 }
